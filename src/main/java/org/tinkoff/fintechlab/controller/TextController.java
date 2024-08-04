61688c1837da72d764b0b243db6db85ec7821231
stukenvitalii <stukenvitalii@gmail.com>
2024-08-04 15:32:49 +0300

    add requests saving to database

--- src/main/java/org/tinkoff/fintechlab/controller/TextController.java
+++ src/main/java/org/tinkoff/fintechlab/controller/TextController.java
@@ -1,6 +1,7 @@
 package org.tinkoff.fintechlab.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.configurationprocessor.json.JSONException;
@@ -10,19 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.tinkoff.fintechlab.configuration.LanguageCode;
+import org.tinkoff.fintechlab.dto.Request;
+import org.tinkoff.fintechlab.service.RequestService;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @Controller
+@AllArgsConstructor
 public class TextController {
     private final TranslateController translateController;
-
     private static final Logger logger = LoggerFactory.getLogger(TextController.class.getName());
-
-
-    public TextController(TranslateController translateController) {
-        this.translateController = translateController;
-    }
+    private final RequestService requestService;
 
     @RequestMapping("/")
     public String home(Model m) {
@@ -37,8 +36,8 @@ public class TextController {
             @RequestParam("textForTranslation") String text,
             Model model,
             HttpServletRequest request
-    ) throws JSONException {
-
+    ) throws JSONException, UnknownHostException {
+        model.addAttribute("languages", LanguageCode.values());
         InetAddress clientIp = getClientIp(request);
 
         if (clientIp != null) {
@@ -52,6 +51,9 @@ public class TextController {
         logger.info("Текст для перевода: {}", text);
 
         String translatedText = translateController.translate(sourceLanguage, targetLanguage, text);
+
+        requestService.add(new Request(clientIp, text, translatedText));
+        logger.info("Saved to DB");
 
         model.addAttribute("sourceText", text);
         model.addAttribute("translatedText", translatedText);
