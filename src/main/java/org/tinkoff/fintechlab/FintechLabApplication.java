5361e39fe1c56d424fed672bc99b2ab1b38baca3
stukenvitalii <stukenvitalii@gmail.com>
2024-08-04 15:19:14 +0300

    add EnableJdbcRepositories

--- src/main/java/org/tinkoff/fintechlab/FintechLabApplication.java
+++ src/main/java/org/tinkoff/fintechlab/FintechLabApplication.java
@@ -3,10 +3,12 @@ package org.tinkoff.fintechlab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.tinkoff.fintechlab.configuration.ApplicationConfig;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
+@EnableJdbcRepositories(basePackages = "org.tinkoff.fintechlab.repository")
 public class FintechLabApplication {
 
     public static void main(String[] args) {
