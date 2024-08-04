23b996af0ccdaf904dfc50189224f5b7999a8586
stukenvitalii <stukenvitalii@gmail.com>
2024-08-04 15:33:24 +0300

    small fix

--- src/main/java/org/tinkoff/fintechlab/repository/RequestRepository.java
+++ src/main/java/org/tinkoff/fintechlab/repository/RequestRepository.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import org.tinkoff.fintechlab.controller.TextController;
 import org.tinkoff.fintechlab.dto.Request;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
