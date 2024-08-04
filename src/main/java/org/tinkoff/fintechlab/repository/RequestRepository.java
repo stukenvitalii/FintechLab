ad08ee944efdb0fb55e25e4e751e44ad644e2b2b
stukenvitalii <stukenvitalii@gmail.com>
2024-08-04 15:20:29 +0300

    add RequestRepository

--- src/main/java/org/tinkoff/fintechlab/repository/RequestRepository.java
+++ src/main/java/org/tinkoff/fintechlab/repository/RequestRepository.java
@@ -1,7 +1,35 @@
 package org.tinkoff.fintechlab.repository;
 
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.tinkoff.fintechlab.controller.TextController;
+import org.tinkoff.fintechlab.dto.Request;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Repository
+@AllArgsConstructor
 public class RequestRepository {
+    private final JdbcClient jdbcClient;
+    private static final Logger logger = LoggerFactory.getLogger(RequestRepository.class.getName());
+
+
+    @Transactional
+    public void add(Request request) throws UnknownHostException {
+        String sql =
+                "INSERT INTO requests (ip_address, input_text, translated_text) VALUES (:ip_address,:input_text,:translated_text)";
+
+//        var inetAddress = InetAddress.getByName("192.168.1.1");
+        var inetAddress = InetAddress.getLocalHost().toString();
+        logger.info("This is IP: " + inetAddress);
+        jdbcClient.sql(sql)
+                .param("ip_address", inetAddress)
+                .param("input_text", request.getInputText())
+                .param("translated_text", request.getTranslatedText())
+                .update();
+    }
 }
