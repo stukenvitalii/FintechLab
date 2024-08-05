e1220cb0673b7a91a884579e552689b1de45b7a4
stukenvitalii <stukenvitalii@gmail.com>
2024-08-05 16:38:39 +0300

    add multithreading

--- src/main/java/org/tinkoff/fintechlab/controller/TextController.java
+++ src/main/java/org/tinkoff/fintechlab/controller/TextController.java
@@ -2,9 +2,9 @@ package org.tinkoff.fintechlab.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.tinkoff.fintechlab.configuration.LanguageCode;
 import org.tinkoff.fintechlab.dto.Request;
+import org.tinkoff.fintechlab.exception.ClientException;
 import org.tinkoff.fintechlab.service.RequestService;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Comparator;
+import java.util.concurrent.*;
 
 @Controller
 @AllArgsConstructor
@@ -36,7 +40,7 @@ public class TextController {
             @RequestParam("textForTranslation") String text,
             Model model,
             HttpServletRequest request
-    ) throws JSONException, UnknownHostException {
+    ) throws UnknownHostException {
         model.addAttribute("languages", LanguageCode.values());
         InetAddress clientIp = getClientIp(request);
 
@@ -72,6 +76,25 @@ public class TextController {
         return "index";
     }
 
+
+    private String getTranslatedTextWithMultithreading(String sourceLanguage, String targetLanguage, String text) throws InterruptedException, ExecutionException, ClientException {
+        CopyOnWriteArrayList<Word> ans = new CopyOnWriteArrayList<>();
+        String[] words = text.split("\\s+");
+
+        try (ExecutorService executor = Executors.newFixedThreadPool(10)) {
+            int index = 0;
+            for (String word : words) {
+                Future<String> completableFuture = executor.submit(() -> translateController.translate(sourceLanguage, targetLanguage, word));
+                ans.add(new Word(completableFuture.get(), index));
+                index++;
+            }
+
+            executor.shutdown();
+            ans.sort(Comparator.comparingInt(Word::getIdx));
+        }
+        return arrayListToString(ans);
+    }
+
     private InetAddress getClientIp(HttpServletRequest request) {
         String remoteAddr = "";
 
@@ -90,5 +113,23 @@ public class TextController {
         }
 
         return inetAddress;
+    }
+
+    private String arrayListToString(CopyOnWriteArrayList<Word> arrayList) {
+        StringBuilder sb = new StringBuilder();
+        for (Word word : arrayList) {
+            if (!sb.isEmpty()) {
+                sb.append(" ");
+            }
+            sb.append(word.getTranslatedWord());
+        }
+        return sb.toString();
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Word {
+        private String translatedWord;
+        private Integer idx;
     }
 }
